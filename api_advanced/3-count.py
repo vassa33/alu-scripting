128051224cd2de244c42197b4eaa26503f291f91
Vanessa Peggy <61325877+vassa33@users.noreply.github.com>
2022-10-21 22:06:09 +0300

    .... fix

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,54 +1,63 @@
 #!/usr/bin/python3
-""""Doc"""
+"""A script that counts the number of occurrences of list of words
+in a given subreddit."""
+
 import requests
 
+headers = {'User-Agent': 'MyAPI/0.0.1'}
 
-def count_words(subreddit, word_list, after="", words_count={}):
-    """"Doc"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=100" \
-        .format(subreddit)
-    header = {'User-Agent': 'Mozilla/5.0'}
-    param = {'after': after}
-    res = requests.get(url, headers=header, params=param)
 
-    if res.status_code != 200:
-        return
+def count_words(subreddit, word_list, after="", hot_list=[]):
+    """print the sorted count of word_list."""
 
-    json_res = res.json()
-    after = json_res.get('data').get('after')
-    has_next = after is not None
-    hot_titles = []
-    print(word_list)
-    words = word_list.lower().split()
+    subreddit_url = "https://reddit.com/r/{}/hot.json".format(subreddit)
 
-    if len(words_count) == 0:
-        words_count = {word: 0 for word in words}
-    # print(words_count)
-    hot_articles = json_res.get('data').get('children')
-    [hot_titles.append(article.get('data').get('title'))
-     for article in hot_articles]
+    parameters = {'limit': 100, 'after': after}
+    response = requests.get(subreddit_url, headers=headers, params=parameters)
 
-    # loop through all titles
-    for i in range(len(hot_titles)):
-        # make the title as a list of word
-        # title_words = hot_titles[i].lower().split()
-        for title_word in hot_titles[i].lower().split():
-            for word in words:
-                if word.lower() == title_word:
-                    words_count[word] = words_count.get(word) + 1
-                # else:
-                #     # pass
-                #     print(word.lower() + " != " + title_word)
+    if response.status_code == 200:
+        json_data = response.json()
+        # get the 'after' value from the response to pass it on the request
 
-    if has_next:
-        # print(after + "\t" + str(has_next))
-        return count_words(subreddit, word_list, after, words_count)
+        # get title and append it to the hot_list
+        for child in json_data.get('data').get('children'):
+            title = child.get('data').get('title')
+            hot_list.append(title)
+
+        # variable after indicates if there is data on the next pagination
+        # on the reddit API after holds a unique name for that subreddit page.
+        # if it is None it indicates it is the last page.
+        after = json_data.get('data').get('after')
+        if after is not None:
+            # print("got next page")
+            # print(len(hot_list))
+            return count_words(subreddit, word_list,
+                               after=after, hot_list=hot_list)
+        else:
+            # put the initial words counter dictionary
+            counter = {}
+            for word in word_list:
+                word = word.lower()
+                if word not in counter.keys():
+                    counter[word] = 0
+                else:
+                    counter[word] += 1
+            # loop through the hot_list to check if word is found in the list
+            for title in hot_list:
+                title_list = title.lower().split(" ")
+                for word in counter.keys():
+
+                    if (word in title_list):
+                        counter[word] += 1
+            sorted_counter = dict(
+                sorted(counter.items(), key=lambda item: item[1]))
+            for key, value in sorted_counter.items():
+                if value > 0:
+                    print("{}: {}".format(key, value))
     else:
-        words_count = dict(filter(lambda item: item[1] != 0,
-                                  words_count.items()))
-        words_count = {key: value for key, value in
-                       sorted(words_count.items(),
-                              key=lambda item: item[1], reverse=True)}
+        return None
 
-    for word, count in words_count.items():
-        print("{}: {}".format(word, count))
+
+if __name__ == '__main__':
+    count_words("programming", ["react", "python", "java",
+                "javascript", "scala", "no_result_for_this"])
